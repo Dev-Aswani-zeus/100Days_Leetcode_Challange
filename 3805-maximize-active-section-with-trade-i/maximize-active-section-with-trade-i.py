d324132375c6bf4339f052e2bc09246f6def6046
Dev-Aswani-zeus <158549454+Dev-Aswani-zeus@users.noreply.github.com>
2026-07-21 20:17:46 +0530

    Time: 686 ms (55.14%) | Memory: 20.6 MB (87.85%) - LeetSync

---
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxActiveSectionsAfterTrade(self, s: str) -> int:
+        ones = 0
+        best = 0
+        prev_zero = float("-inf")
+        i = 0
+        n = len(s)
+        while i < n:
+            j = i
+            while j < n and s[j] == s[i]:
+                j += 1
+            length = j - i
+            if s[i] == '1':
+                ones += length
+            else:
+                best = max(best, prev_zero + length)
+                prev_zero = length
+            i = j
+        return ones + best
